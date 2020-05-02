ce26796f08f2d4be5b0411dfe82fc8f92ad4ad2d
Sachi Kulkarni <sgkulkarni@berkeley.edu>
2020-05-02 01:29:19 -0700

    fixed invalid submission ugh

--- solver.py
+++ solver.py
@@ -1,7 +1,8 @@
 import networkx as nx
-from parse import read_input_file, write_output_file
+from parse import read_input_file, write_output_file, read_output_file
 from utils import is_valid_network, average_pairwise_distance
 import sys
+import os
 
 
 def solve(G):
@@ -15,14 +16,20 @@ def solve(G):
 
     # TODO: your code here!
     # T1 = G.copy()
-    if DegreeCheck(G):
-    	return G
+    # if DegreeCheck(G):
+    # 	return G
 
-    T = nx.minimum_spanning_tree(G)
-    T2 = removeLeaves(T)
+    if (G.number_of_nodes() == 2):
+    	T2 = nx.Graph()
+    	T2.add_node(0)
+    
+    else:
+
+	    T = nx.minimum_spanning_tree(G)
+	    T2 = removeLeaves(T)
 
     return T2
-
+    
 def DegreeCheck(G):
 	numNodes = G.number_of_nodes()
 
@@ -39,8 +46,7 @@ def DegreeCheck(G):
 
 
 def removeLeaves(G):
-	# print(G.edges(data = True))
-	
+
 	vertexOccurences = {}
 
 	for edge in G.edges(data = True):
@@ -62,30 +68,15 @@ def removeLeaves(G):
 			vertexOccurences[secondVertex] = 1
 
 
-	# print(vertexOccurences)
-	
-
 	verticesToBeRemoved = []
 	for vertex in vertexOccurences:
 		if vertexOccurences[vertex] == 1:
 			#remove this vertex
 			verticesToBeRemoved.append(vertex)
 
-	# newEdges = []
-
-
-	# for edge in G.edges(data = True):
-	# 	if edge[0] not in verticesToBeRemoved and edge[1] not in verticesToBeRemoved:
-	# 		newEdges.append(edge)
-	# 	else:
-	# 		pass
-
 
 	for vertex in verticesToBeRemoved:
 		G.remove_node(vertex)
-
-	# print(G.edges())
-	# print(G.nodes())
 	return G
 
 
@@ -94,11 +85,26 @@ def removeLeaves(G):
 
 # Usage: python3 solver.py test.in
 
-if __name__ == '__main__':
-    assert len(sys.argv) == 2
-    path = sys.argv[1]
-    G = read_input_file(path)
-    T = solve(G)
-    assert is_valid_network(G, T)
-    print("Average pairwise distance: {}".format(average_pairwise_distance(T)))
-    write_output_file(T, 'out/test3.out')
+# if __name__ == '__main__':
+#     assert len(sys.argv) == 2
+#     path = sys.argv[1]
+#     G = read_input_file(path)
+#     T = solve(G)
+#     # print(read_output_file(path, G))
+#     assert is_valid_network(G, T)
+   
+#     print("Average pairwise distance: {}".format(average_pairwise_distance(T)))
+#     write_output_file(T, 'out/test2.out')
+
+if __name__ == "__main__":
+    output_dir = "outputs"
+    input_dir = "inputs"
+    for input_path in os.listdir(input_dir):
+        graph_name = input_path.split(".")[0]
+        G = read_input_file(f"{input_dir}/{input_path}")
+        T = solve(G)
+        # print(input_path)
+        assert is_valid_network(G, T)
+        write_output_file(T, f"{output_dir}/{graph_name}.out")
+
+
